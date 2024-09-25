109d72b1bf8bf313466c0088c55f542cd9d5d97e
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-09-25 16:44:48 +0100

    Add order history to profile and checkout default address

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 def profile(request):
     """ Display the user's profile. """
@@ -21,4 +22,17 @@ def profile(request):
         'orders': orders,
     }
     
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """ Display the user's order history. """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
     return render(request, template, context)
