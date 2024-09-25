e8bfec2f90b83b7d0aaf4cfaa52cddd5eb9dd779
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-09-25 21:43:11 +0100

    Add add_product view and template

--- products/urls.py
+++ products/urls.py
@@ -7,7 +7,8 @@ from django.conf import settings
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('products/vote/<int:product_id>/', product_vote, name='product-vote'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
 
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Product, Vote
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
 from .models import Product, Category
+from .forms import ProductForm
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -105,3 +107,28 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+@login_required
+def add_product(request):
+    """ Add a product to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
