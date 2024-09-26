60389b0995a031ef49cfa6eed48141065bbb07dc
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-09-26 17:30:58 +0100

    Removed secret keys and set debug

--- natures_elixir/settings.py
+++ natures_elixir/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-73w)z$psrf677-i5q2(zdl=4-rwek=ol&(+=l$n0$ig7*g0#73'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
