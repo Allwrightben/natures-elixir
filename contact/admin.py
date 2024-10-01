334d251953c625e488a5a532530a2204a2c5b219
Allwrightben <ben.allwright@learningpeople.co.uk>
2024-10-01 18:56:07 +0100

    Complate contact app, and footer with FB link and newsletter

--- contact/admin.py
+++ contact/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from .models import Contact
 
+
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'category', 'message_short', 'id')
     list_filter = ('category',)
     search_fields = ('name', 'email', 'message')
 
     def message_short(self, obj):
-        return obj.message[:50] + '...' if len(obj.message) > 50 else obj.message
+        return (
+            obj.message[:50] + '...' if len(obj.message) > 50 else obj.message
+        )
 
     message_short.short_description = 'Message'
+
 
 admin.site.register(Contact, ContactAdmin)
